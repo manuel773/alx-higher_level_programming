8087fc19a83f2d14a43d57a2b2f0e85cefc8039e
root <emma.smart@gg>
2022-08-31 19:24:34 -0700

    hot fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for key in a_dictionary:
-        new_dict[key] = a_dictionary[key] + a_dictionary[key]
-        return new_dict
+    return {key: val * 2 for key, val in a_dictionary.items()}
